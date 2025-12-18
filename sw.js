089be7e7ffe4001972034d78d327b8c0d7b9fd5a
Bernio <jendabernard@gmail.com>
2025-12-18 12:35:11 +0100

    Revise service worker for caching and assets
    Updated service worker to include additional assets and cache version.

--- sw.js
+++ sw.js
@@ -1,17 +1,23 @@
-const CACHE = "barvy-cache-v15";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const CACHE = "barvy-v7"; // pokaždé zvyš číslo
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      "./",
+      "./index.html",
+      "./manifest.json",
+      "./video.mp4",
+      "./icon-192.png",
+      "./icon-512.png"
+    ]))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
