4a8390478bdd804763c43243218fcdc8beb7ca51
Bernio <jendabernard@gmail.com>
2025-12-22 19:24:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 'use strict';
 
 // ✅ Tohle změň při každém nasazení nové verze
-const APP_VERSION = '2025-12-18_15';
+const APP_VERSION = '2025-12-18_16';
 
 const CACHE_PREFIX = 'svetla';
 const CACHE_HTML   = `${CACHE_PREFIX}-html-${APP_VERSION}`;
